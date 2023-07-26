835a1b2ae123085f4c4ea137bf70ee1b2b31268f
Ilya Agafonov <tairesh.rus@gmail.com>
2023-07-26 17:22:02 +0500

    little bit more nice changelog posts

--- changelog/tgbot.py
+++ changelog/tgbot.py
@@ -3,6 +3,7 @@ import os
 import re
 import traceback
 from typing import List
+from datetime import datetime
 
 from telebot import TeleBot
 
@@ -10,8 +11,13 @@ from changelog import published
 from changelog.github import Release
 
 ADMIN = 31445050
+CHANGELOG_CHANNEL = -1001332994456
 GITHUB_ISSUE_URL_TEMPLATE = "https://github.com/CleverRaven/Cataclysm-DDA/issues/{}"
 r_hashtag = re.compile(r'#([\d]+)')
+HARDCODED_STRINGS = {
+    "## What's Changed": "<b>What's changed:</b>",
+    "**Full Changelog**:": "<b>Full Changelog:</b>",
+}
 
 
 def _prepare(text: str) -> str:
@@ -46,12 +52,16 @@ def _message_to_bundles(message: str) -> List[str]:
 
 def get_bundles(release: Release) -> List[str]:
     bundles = []
-    message = f'<a href="{release.url}"><b>{release.name}</b></a> {release.created_at}\n\n'
+    created_at = datetime.strptime(release.created_at, '%Y-%m-%dT%H:%M:%SZ')
+    message = f'<a href="{release.url}"><b>{release.name}</b></a> {created_at}\n\n'
 
     if release.description:
         message += release.description
     else:
         message += '<i>(no changes)</i>'
+
+    for (string, sub) in HARDCODED_STRINGS.items():
+        message = message.replace(string, sub)
 
     bundles += _message_to_bundles(message)
     return bundles
@@ -67,7 +77,7 @@ def send(release: Release):
 
     try:
         for bundle in bundles:
-            bot.send_message(-1001332994456, bundle, parse_mode='HTML')
+            bot.send_message(CHANGELOG_CHANNEL, bundle, parse_mode='HTML')
         published.save(release.id)
     except Exception:
         bot.send_message(ADMIN, traceback.format_exc())
