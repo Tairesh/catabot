9611ebfa58145442bcb24cb60c2a8f6859816f66
Ilia Agafonov <tairesh.rus@gmail.com>
2024-06-20 15:38:32 +0300

    :bug: Fix download_data for new Norndragon repository structure

--- download_data/__init__.py
+++ download_data/__init__.py
@@ -3,5 +3,4 @@ from os import path
 ROOT_DIR = path.dirname(path.dirname(__file__))
 DATA_VERSION_FILE = path.join(ROOT_DIR, "data_version.txt")
 ALL_DATA_FILE = path.join(ROOT_DIR, "data.json")
-LAST_VERSION_URL = "https://raw.githubusercontent.com/nornagon/cdda-data/main/latest-build.json"
-ALL_DATA_URL = "https://raw.githubusercontent.com/nornagon/cdda-data/main/data/{}/all.json"
+ALL_DATA_URL = "https://raw.githubusercontent.com/nornagon/cdda-data/main/data/latest/all.json"
--- download_data/__main__.py
+++ download_data/__main__.py
@@ -1,10 +1,9 @@
 import shutil
 from os import path
-import json
 from urllib import request
 from typing import Optional
 
-from . import DATA_VERSION_FILE, ALL_DATA_FILE, LAST_VERSION_URL, ALL_DATA_URL
+from . import DATA_VERSION_FILE, ALL_DATA_FILE, ALL_DATA_URL
 
 
 def current_data_version() -> Optional[str]:
@@ -14,20 +13,11 @@ def current_data_version() -> Optional[str]:
     return None
 
 
-def last_version() -> str:
-    data = json.loads(request.urlopen(LAST_VERSION_URL).read())
-    return data['latest_build']
-
-
-def download_data(version):
-    if version != current_data_version():
-        print(f"downloading {version}...")
-        with open(ALL_DATA_FILE, 'wb') as data_file, \
-                open(DATA_VERSION_FILE, 'w') as version_file, \
-                request.urlopen(ALL_DATA_URL.format(version)) as response:
-            shutil.copyfileobj(response, data_file)
-            version_file.write(version)
+def download_data():
+    with open(ALL_DATA_FILE, 'wb') as data_file, \
+            request.urlopen(ALL_DATA_URL) as response:
+        shutil.copyfileobj(response, data_file)
 
 
 if __name__ == "__main__":
-    download_data(last_version())
+    download_data()
